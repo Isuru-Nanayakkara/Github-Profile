e2950f4264d11407c2ee98698c273b2cdcb517ec
Isuru Nanayakkara <isuru@cenango.lk>
2021-06-09 17:43:59 +0530

    Removed storyboard file. Added app icon.

---
+++ GithubProfile/GithubProfile/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GithubProfile
+//
+//  Created by Isuru Nanayakkara on 2021-06-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGreen
+    }
+    
+}
